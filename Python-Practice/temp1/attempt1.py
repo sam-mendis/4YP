e0e46a9323d85ce38e322a81b6562b8b040a5c70
Sam Mendis <samuel.mendis@lincoln.ox.ac.uk>
2020-12-01 11:12:31 +0000

    Update attempt1.py

--- Python-Practice/temp1/attempt1.py
+++ Python-Practice/temp1/attempt1.py
@@ -3,9 +3,11 @@ from tkinter import *
 # need to have this before any tkinter program.
 # this creates the window for the gui
 root = Tk()
-# creating a label widget
-myLabel = Label(root, text="Hello World")
-# shoving the label onto the screen
-myLabel.pack()
+# creating a labels
+myLabel1 = Label(root, text="Temperature Controls")
+myLabel2 = Label(root, text="Temp/K")
+# using the grid
+myLabel1.grid(row=0, column=1)
+myLabel2.grid(row=1, column=0)
 
 root.mainloop()
