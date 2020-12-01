4dde17eaa6c531d87a65fba089c6eaeb7c766687
Sam Mendis <samuel.mendis@lincoln.ox.ac.uk>
2020-12-01 12:10:32 +0000

    Update attempt1.py

--- Python-Practice/temp1/attempt1.py
+++ Python-Practice/temp1/attempt1.py
@@ -3,12 +3,21 @@ from tkinter import *
 # need to have this before any tkinter program.
 # this creates the window for the gui
 root = Tk()
+
+
+# Creating a function for the button
+def myClick():
+    mylabel = Label(root, text="Final Temp")
+    mylabel.grid(row=1, column=3)
+
+
 # creating a labels
 myLabel1 = Label(root, text="Temperature Controls")
 myLabel2 = Label(root, text="Temp/K")
 
-# Creating a button
-button_confirm = Button(root, text="Confirm", padx=50, pady=50)
+# Creating a button, command calls the function next to command
+# remember when using the command button not to but brackets after the function
+button_confirm = Button(root, text="Confirm", padx=50, command=myClick)
 # using the grid to place pobjects
 myLabel1.grid(row=0, column=1)
 myLabel2.grid(row=1, column=0)
