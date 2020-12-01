33f8cc4403d786917001b8a2269112689073c95e
Sam Mendis <samuel.mendis@lincoln.ox.ac.uk>
2020-12-01 11:24:07 +0000

    Update attempt1.py

--- Python-Practice/temp1/attempt1.py
+++ Python-Practice/temp1/attempt1.py
@@ -6,8 +6,13 @@ root = Tk()
 # creating a labels
 myLabel1 = Label(root, text="Temperature Controls")
 myLabel2 = Label(root, text="Temp/K")
-# using the grid
+
+# Creating a button
+button_confirm = Button(root, text="Confirm", padx=50, pady=50)
+# using the grid to place pobjects
 myLabel1.grid(row=0, column=1)
 myLabel2.grid(row=1, column=0)
+button_confirm.grid(row=1, column=2)
+
 
 root.mainloop()
