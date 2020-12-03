f0accce167cf1ce88c78b14e04eb7b0638138b77
Sam Mendis <samuel.mendis@lincoln.ox.ac.uk>
2020-12-03 11:58:17 +0000

    Update attempt1.py

--- Python-Practice/temp1/attempt1.py
+++ Python-Practice/temp1/attempt1.py
@@ -12,49 +12,47 @@ frame_s.grid()
 
 
 # Creating the input table
-inputlabel = Label(frame_s, text="Manual Input", bd=1,
-                   relief="solid", font=" Times 16")
+inputlabel = Label(frame_s, text="Manual Input", font=" Times 16", width=35)
 inputlabel.grid(row=0, column=0, columnspan=6)
 templabel = Label(
-    frame_s, text="Temp/\N{DEGREE SIGN}C", bd=1, relief="solid", font=" Times 14", width=7)
-templabel.grid(row=1, column=0, rowspan=2)
+    frame_s, text="Temp/\N{DEGREE SIGN}C", font=" Times 14", width=7)
+templabel.grid(row=2, column=0)
 
 
 timelabel = Label(frame_s, text="Time", bd=1, relief="solid",
-                  font=" Times 14", width=7)
+                  font=" Times 14", width=26)
 timelabel.grid(row=1, column=1, columnspan=3)
-timelabeldays = Label(frame_s, text="Days", bd=1,
-                      relief="solid", font=" Times 12", width=7)
+timelabeldays = Label(frame_s, text="Days", font=" Times 14", width=7)
 timelabeldays.grid(row=2, column=1)
-timelabelhours = Label(frame_s, text="Hours", bd=1,
-                       relief="solid", font=" Times 12", width=7)
+timelabelhours = Label(frame_s, text="Hours", font=" Times 14", width=7)
 timelabelhours.grid(row=2, column=2)
-timelabelmins = Label(frame_s, text="Minutes", bd=1,
-                      relief="solid", font=" Times 12", width=7)
+timelabelmins = Label(frame_s, text="Minutes", font=" Times 14", width=7)
 timelabelmins.grid(row=2, column=3)
 
-gas1label = Label(frame_s, text="Gas A %", bd=1,
-                  relief="solid", font=" Times 14", width=7)
-gas1label.grid(row=1, column=4, rowspan=2)
-gas2label = Label(frame_s, text="Gas B %", bd=1,
-                  relief="solid", font=" Times 14", width=7)
-gas2label.grid(row=1, column=5, rowspan=2)
+gas1label = Label(frame_s, text="Gas A %", font=" Times 14", width=7)
+gas1label.grid(row=2, column=4,)
+gas2label = Label(frame_s, text="Gas B %", font=" Times 14", width=7)
+gas2label.grid(row=2, column=5)
 
-e_temp = Entry(frame_s, bd=1, relief="solid", width=7)
+e_temp = Entry(frame_s, bd=1, relief="solid", font=" Times 14", width=7)
 e_temp.grid(row=3, column=0)
 
-e_timed = Entry(frame_s, width=7)
+e_timed = Entry(frame_s, bd=1, relief="solid", font=" Times 14", width=7)
 e_timed.grid(row=3, column=1)
-e_timeh = Entry(frame_s, width=7)
+e_timeh = Entry(frame_s, bd=1, relief="solid", font=" Times 14", width=7)
 e_timeh.grid(row=3, column=2)
-e_timem = Entry(frame_s, width=7)
+e_timem = Entry(frame_s, bd=1, relief="solid", font=" Times 14", width=7)
 e_timem.grid(row=3, column=3)
 
-e_gas1 = Entry(frame_s, width=7)
+e_gas1 = Entry(frame_s, bd=1, relief="solid", font=" Times 14", width=7)
 e_gas1.grid(row=3, column=4)
-e_gas2 = Entry(frame_s, width=7)
+e_gas2 = Entry(frame_s, bd=1, relief="solid", font=" Times 14", width=7)
 e_gas2.grid(row=3, column=5)
 
+# Creating a button, command calls the function next to command
+# remember when using the command button not to but brackets after the function
+button_next = Button(frame_s, text="Next", command=next)
+button_next.grid(row=3, column=6)
 
 # Creating entries to input data
 #e_t = Entry(root, width=50, borderwidth=5)
@@ -65,6 +63,8 @@ e_gas2.grid(row=3, column=5)
 # Creating a function for the button
 
 # making sure you are collecting the entry using e.get
+
+
 def next():
     global temp_i, timed_i, timeh_i, timem_i
     temp_i = e_temp.get()
@@ -90,13 +90,6 @@ def next():
 
 
 # creating a labels
-# Creating a button, command calls the function next to command
-# remember when using the command button not to but brackets after the function
-button_confirm = Button(frame_s, text="Confirm", command=next)
-
-
-# using the grid to place pobjects
-button_confirm.grid(row=2, column=6, rowspan=2)
 
 
 root.mainloop()
